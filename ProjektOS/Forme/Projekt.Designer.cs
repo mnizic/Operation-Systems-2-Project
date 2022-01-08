2070f81c85b368025e0418ca9168675d5dcb7cec
Mario Nižić <mnizic@foi.hr>
2022-01-08 22:09:57 +0100

    Uređeno sučelje i uklonjene neke nebitne stavke.

--- ProjektOS/Forme/Projekt.Designer.cs
+++ ProjektOS/Forme/Projekt.Designer.cs
@@ -31,55 +31,50 @@ namespace ProjektOS
         {
             this.stvoriNovuDatotekuButton = new System.Windows.Forms.Button();
             this.otvoriDatotekuButton = new System.Windows.Forms.Button();
-            this.otvoriDatotekuSadrzaj = new System.Windows.Forms.TextBox();
             this.simetricnoRadio = new System.Windows.Forms.RadioButton();
             this.asimetricnoRadio = new System.Windows.Forms.RadioButton();
             this.enkriptirajButton = new System.Windows.Forms.Button();
             this.dekriptirajButton = new System.Windows.Forms.Button();
-            this.enkriptiraniSadrzaj = new System.Windows.Forms.TextBox();
-            this.dekriptiraniSadrzaj = new System.Windows.Forms.TextBox();
+            this.obicanTekstSadrzaj = new System.Windows.Forms.TextBox();
             this.sazetakTextBox = new System.Windows.Forms.TextBox();
             this.otvoriKriptiraniTekst = new System.Windows.Forms.Button();
             this.ocistiButton = new System.Windows.Forms.Button();
-            this.hashButton = new System.Windows.Forms.Button();
             this.digitalniPotpisTextBox = new System.Windows.Forms.TextBox();
             this.digitalniPotpisButton = new System.Windows.Forms.Button();
             this.provjeraButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.enkriptiraniSadrzaj = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // stvoriNovuDatotekuButton
             // 
-            this.stvoriNovuDatotekuButton.Location = new System.Drawing.Point(12, 12);
+            this.stvoriNovuDatotekuButton.Location = new System.Drawing.Point(15, 32);
             this.stvoriNovuDatotekuButton.Name = "stvoriNovuDatotekuButton";
-            this.stvoriNovuDatotekuButton.Size = new System.Drawing.Size(130, 36);
+            this.stvoriNovuDatotekuButton.Size = new System.Drawing.Size(154, 36);
             this.stvoriNovuDatotekuButton.TabIndex = 0;
-            this.stvoriNovuDatotekuButton.Text = "Stvori datoteku";
+            this.stvoriNovuDatotekuButton.Text = "Stvori novu datoteku";
             this.stvoriNovuDatotekuButton.UseVisualStyleBackColor = true;
             this.stvoriNovuDatotekuButton.Click += new System.EventHandler(this.stvoriNovuDatotekuButton_Click);
             // 
             // otvoriDatotekuButton
             // 
-            this.otvoriDatotekuButton.Location = new System.Drawing.Point(12, 54);
+            this.otvoriDatotekuButton.Location = new System.Drawing.Point(175, 32);
             this.otvoriDatotekuButton.Name = "otvoriDatotekuButton";
-            this.otvoriDatotekuButton.Size = new System.Drawing.Size(130, 36);
+            this.otvoriDatotekuButton.Size = new System.Drawing.Size(154, 36);
             this.otvoriDatotekuButton.TabIndex = 1;
             this.otvoriDatotekuButton.Text = "Otvori datoteku";
             this.otvoriDatotekuButton.UseVisualStyleBackColor = true;
             this.otvoriDatotekuButton.Click += new System.EventHandler(this.otvoriDatotekuButton_Click);
             // 
-            // otvoriDatotekuSadrzaj
-            // 
-            this.otvoriDatotekuSadrzaj.Location = new System.Drawing.Point(161, 12);
-            this.otvoriDatotekuSadrzaj.Multiline = true;
-            this.otvoriDatotekuSadrzaj.Name = "otvoriDatotekuSadrzaj";
-            this.otvoriDatotekuSadrzaj.Size = new System.Drawing.Size(216, 78);
-            this.otvoriDatotekuSadrzaj.TabIndex = 2;
-            // 
             // simetricnoRadio
             // 
             this.simetricnoRadio.AutoSize = true;
+            this.simetricnoRadio.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.simetricnoRadio.Checked = true;
-            this.simetricnoRadio.Location = new System.Drawing.Point(47, 213);
+            this.simetricnoRadio.Location = new System.Drawing.Point(383, 148);
             this.simetricnoRadio.Name = "simetricnoRadio";
             this.simetricnoRadio.Size = new System.Drawing.Size(95, 21);
             this.simetricnoRadio.TabIndex = 3;
@@ -90,7 +85,7 @@ namespace ProjektOS
             // asimetricnoRadio
             // 
             this.asimetricnoRadio.AutoSize = true;
-            this.asimetricnoRadio.Location = new System.Drawing.Point(148, 213);
+            this.asimetricnoRadio.Location = new System.Drawing.Point(530, 148);
             this.asimetricnoRadio.Name = "asimetricnoRadio";
             this.asimetricnoRadio.Size = new System.Drawing.Size(102, 21);
             this.asimetricnoRadio.TabIndex = 4;
@@ -99,7 +94,7 @@ namespace ProjektOS
             // 
             // enkriptirajButton
             // 
-            this.enkriptirajButton.Location = new System.Drawing.Point(12, 240);
+            this.enkriptirajButton.Location = new System.Drawing.Point(442, 106);
             this.enkriptirajButton.Name = "enkriptirajButton";
             this.enkriptirajButton.Size = new System.Drawing.Size(130, 36);
             this.enkriptirajButton.TabIndex = 5;
@@ -109,7 +104,7 @@ namespace ProjektOS
             // 
             // dekriptirajButton
             // 
-            this.dekriptirajButton.Location = new System.Drawing.Point(148, 240);
+            this.dekriptirajButton.Location = new System.Drawing.Point(442, 171);
             this.dekriptirajButton.Name = "dekriptirajButton";
             this.dekriptirajButton.Size = new System.Drawing.Size(130, 36);
             this.dekriptirajButton.TabIndex = 6;
@@ -117,28 +112,19 @@ namespace ProjektOS
             this.dekriptirajButton.UseVisualStyleBackColor = true;
             this.dekriptirajButton.Click += new System.EventHandler(this.dekriptirajButton_Click);
             // 
-            // enkriptiraniSadrzaj
-            // 
-            this.enkriptiraniSadrzaj.Enabled = false;
-            this.enkriptiraniSadrzaj.Location = new System.Drawing.Point(12, 106);
-            this.enkriptiraniSadrzaj.Multiline = true;
-            this.enkriptiraniSadrzaj.Name = "enkriptiraniSadrzaj";
-            this.enkriptiraniSadrzaj.Size = new System.Drawing.Size(365, 101);
-            this.enkriptiraniSadrzaj.TabIndex = 7;
-            // 
             // dekriptiraniSadrzaj
             // 
-            this.dekriptiraniSadrzaj.Enabled = false;
-            this.dekriptiraniSadrzaj.Location = new System.Drawing.Point(12, 292);
-            this.dekriptiraniSadrzaj.Multiline = true;
-            this.dekriptiraniSadrzaj.Name = "dekriptiraniSadrzaj";
-            this.dekriptiraniSadrzaj.Size = new System.Drawing.Size(365, 101);
-            this.dekriptiraniSadrzaj.TabIndex = 8;
+            this.obicanTekstSadrzaj.Enabled = false;
+            this.obicanTekstSadrzaj.Location = new System.Drawing.Point(12, 106);
+            this.obicanTekstSadrzaj.Multiline = true;
+            this.obicanTekstSadrzaj.Name = "dekriptiraniSadrzaj";
+            this.obicanTekstSadrzaj.Size = new System.Drawing.Size(365, 101);
+            this.obicanTekstSadrzaj.TabIndex = 8;
             // 
             // sazetakTextBox
             // 
             this.sazetakTextBox.Enabled = false;
-            this.sazetakTextBox.Location = new System.Drawing.Point(383, 12);
+            this.sazetakTextBox.Location = new System.Drawing.Point(256, 291);
             this.sazetakTextBox.Multiline = true;
             this.sazetakTextBox.Name = "sazetakTextBox";
             this.sazetakTextBox.Size = new System.Drawing.Size(513, 32);
@@ -146,9 +132,9 @@ namespace ProjektOS
             // 
             // otvoriKriptiraniTekst
             // 
-            this.otvoriKriptiraniTekst.Location = new System.Drawing.Point(383, 106);
+            this.otvoriKriptiraniTekst.Location = new System.Drawing.Point(640, 26);
             this.otvoriKriptiraniTekst.Name = "otvoriKriptiraniTekst";
-            this.otvoriKriptiraniTekst.Size = new System.Drawing.Size(130, 48);
+            this.otvoriKriptiraniTekst.Size = new System.Drawing.Size(129, 48);
             this.otvoriKriptiraniTekst.TabIndex = 10;
             this.otvoriKriptiraniTekst.Text = "Otvori kriptiranu datoteku";
             this.otvoriKriptiraniTekst.UseVisualStyleBackColor = true;
@@ -156,36 +142,26 @@ namespace ProjektOS
             // 
             // ocistiButton
             // 
-            this.ocistiButton.Location = new System.Drawing.Point(383, 160);
+            this.ocistiButton.Location = new System.Drawing.Point(442, 214);
             this.ocistiButton.Name = "ocistiButton";
-            this.ocistiButton.Size = new System.Drawing.Size(130, 48);
+            this.ocistiButton.Size = new System.Drawing.Size(130, 36);
             this.ocistiButton.TabIndex = 11;
             this.ocistiButton.Text = "Očisti sve";
             this.ocistiButton.UseVisualStyleBackColor = true;
             this.ocistiButton.Click += new System.EventHandler(this.ocistiButton_Click);
             // 
-            // hashButton
-            // 
-            this.hashButton.Location = new System.Drawing.Point(766, 50);
-            this.hashButton.Name = "hashButton";
-            this.hashButton.Size = new System.Drawing.Size(130, 36);
-            this.hashButton.TabIndex = 12;
-            this.hashButton.Text = "Hash";
-            this.hashButton.UseVisualStyleBackColor = true;
-            this.hashButton.Click += new System.EventHandler(this.hashButton_Click);
-            // 
             // digitalniPotpisTextBox
             // 
             this.digitalniPotpisTextBox.Enabled = false;
-            this.digitalniPotpisTextBox.Location = new System.Drawing.Point(527, 107);
+            this.digitalniPotpisTextBox.Location = new System.Drawing.Point(256, 365);
             this.digitalniPotpisTextBox.Multiline = true;
             this.digitalniPotpisTextBox.Name = "digitalniPotpisTextBox";
-            this.digitalniPotpisTextBox.Size = new System.Drawing.Size(365, 101);
+            this.digitalniPotpisTextBox.Size = new System.Drawing.Size(513, 101);
             this.digitalniPotpisTextBox.TabIndex = 13;
             // 
             // digitalniPotpisButton
             // 
-            this.digitalniPotpisButton.Location = new System.Drawing.Point(762, 214);
+            this.digitalniPotpisButton.Location = new System.Drawing.Point(256, 472);
             this.digitalniPotpisButton.Name = "digitalniPotpisButton";
             this.digitalniPotpisButton.Size = new System.Drawing.Size(130, 36);
             this.digitalniPotpisButton.TabIndex = 14;
@@ -195,7 +171,7 @@ namespace ProjektOS
             // 
             // provjeraButton
             // 
-            this.provjeraButton.Location = new System.Drawing.Point(762, 256);
+            this.provjeraButton.Location = new System.Drawing.Point(392, 472);
             this.provjeraButton.Name = "provjeraButton";
             this.provjeraButton.Size = new System.Drawing.Size(130, 36);
             this.provjeraButton.TabIndex = 15;
@@ -203,25 +179,72 @@ namespace ProjektOS
             this.provjeraButton.UseVisualStyleBackColor = true;
             this.provjeraButton.Click += new System.EventHandler(this.provjeraButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Enkriptirani tekst";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Običan tekst";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Sažetak običnog teksta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(253, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Digitalni potpis";
+            // 
+            // enkriptiraniSadrzaj
+            // 
+            this.enkriptiraniSadrzaj.Enabled = false;
+            this.enkriptiraniSadrzaj.Location = new System.Drawing.Point(638, 106);
+            this.enkriptiraniSadrzaj.Multiline = true;
+            this.enkriptiraniSadrzaj.Name = "enkriptiraniSadrzaj";
+            this.enkriptiraniSadrzaj.Size = new System.Drawing.Size(365, 101);
+            this.enkriptiraniSadrzaj.TabIndex = 7;
+            // 
             // Projekt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(904, 450);
+            this.ClientSize = new System.Drawing.Size(1027, 535);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.provjeraButton);
             this.Controls.Add(this.digitalniPotpisButton);
             this.Controls.Add(this.digitalniPotpisTextBox);
-            this.Controls.Add(this.hashButton);
             this.Controls.Add(this.ocistiButton);
             this.Controls.Add(this.otvoriKriptiraniTekst);
             this.Controls.Add(this.sazetakTextBox);
-            this.Controls.Add(this.dekriptiraniSadrzaj);
+            this.Controls.Add(this.obicanTekstSadrzaj);
             this.Controls.Add(this.enkriptiraniSadrzaj);
             this.Controls.Add(this.dekriptirajButton);
             this.Controls.Add(this.enkriptirajButton);
             this.Controls.Add(this.asimetricnoRadio);
             this.Controls.Add(this.simetricnoRadio);
-            this.Controls.Add(this.otvoriDatotekuSadrzaj);
             this.Controls.Add(this.otvoriDatotekuButton);
             this.Controls.Add(this.stvoriNovuDatotekuButton);
             this.Name = "Projekt";
@@ -236,20 +259,22 @@ namespace ProjektOS
 
         private System.Windows.Forms.Button stvoriNovuDatotekuButton;
         private System.Windows.Forms.Button otvoriDatotekuButton;
-        private System.Windows.Forms.TextBox otvoriDatotekuSadrzaj;
         private System.Windows.Forms.RadioButton simetricnoRadio;
         private System.Windows.Forms.RadioButton asimetricnoRadio;
         private System.Windows.Forms.Button enkriptirajButton;
         private System.Windows.Forms.Button dekriptirajButton;
-        private System.Windows.Forms.TextBox enkriptiraniSadrzaj;
-        private System.Windows.Forms.TextBox dekriptiraniSadrzaj;
+        private System.Windows.Forms.TextBox obicanTekstSadrzaj;
         private System.Windows.Forms.TextBox sazetakTextBox;
         private System.Windows.Forms.Button otvoriKriptiraniTekst;
         private System.Windows.Forms.Button ocistiButton;
-        private System.Windows.Forms.Button hashButton;
         private System.Windows.Forms.TextBox digitalniPotpisTextBox;
         private System.Windows.Forms.Button digitalniPotpisButton;
         private System.Windows.Forms.Button provjeraButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox enkriptiraniSadrzaj;
     }
 }
 
